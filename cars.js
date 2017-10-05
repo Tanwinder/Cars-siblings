89d818f796df0d5ef73578442ac12dd470ad2a9e
TanSidhu <sidhutanu90@gmail.com>
2017-10-04 21:05:32 -0400

    add html

--- cars.js
+++ cars.js
@@ -2,6 +2,19 @@ document.querySelector(".parent-list").addEventListener("click",function(e){
 
   var elem = e.target;
   elem.classList.toggle('add-border');
+  var content = elem.textContent;
+  var contentNode = document.querySelector('.content-container');
+  contentNode.innerHTML = content;
+  contentNode.classList.remove('start-animation');
+  // -> triggering reflow /* The actual magic */
+ // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+ // This was, from the original tutorial, will no work in strict mode. Thanks Felis Phasma! The next uncommented line is the fix.
+ // element.offsetWidth = element.offsetWidth;
+
+ void contentNode.offsetWidth;
+
+ // -> and re-adding the class
+  contentNode.classList.add('start-animation');
   var getSiblings = function (elem) {
     var siblings = [];
     var sibling = elem.parentNode.firstChild;
@@ -10,9 +23,9 @@ document.querySelector(".parent-list").addEventListener("click",function(e){
         siblings.push(sibling);
     }
    return siblings;
-};
+ };
   var siblings = getSiblings(elem);
   siblings.map(function(sib){
     sib.classList.remove('add-border')
-  })
+  });
 });
